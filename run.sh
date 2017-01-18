5390e9e8b53dca2910548862f79c0079fdadb1f6
Bradley Cicenas <bradley.cicenas@gmail.com>
2017-01-18 20:29:32 +0000

    update run wrapper to remove deprecated config var

--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -e
 
-[ -z "$ES_HEAP_SIZE" ] && {
+[ "$HEAP_SIZE" == "auto" ] && {
   total_mem=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
   alloc_bytes=$(echo "$total_mem * .9" | bc | cut -f1 -d\.)
-  ES_HEAP_SIZE=$((alloc_bytes /1024/1024))M
+  export HEAP_SIZE="$((alloc_bytes /1024/1024))m"
 }
 
+ES_JAVA_OPTS+="-Xms${HEAP_SIZE} -Xmx${HEAP_SIZE}"
+
 echo "Starting Elasticsearch..."
-echo "Heap Size: $ES_HEAP_SIZE"
+echo "ES_JAVA_OPTS: $ES_JAVA_OPTS"
 
 exec /opt/elasticsearch/bin/elasticsearch -Des.logger.level=$ELASTICSEARCH_LOG_LEVEL \
                                           -Des.default.path.logs=${LOG_DIR} \
