4fab0cf23180cf334ea35caf39f9d95491feea5f
Bradley Cicenas <bradley.cicenas@gmail.com>
2017-01-19 16:53:22 +0000

    update wrapper, config for 5.1.2

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,11 @@ ENV ES_VERSION 5.1.2
 ENV ES_URL https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ES_VERSION}.tar.gz
 
 ENV HEAP_SIZE auto
-ENV LOG_DIR /elastic/log/
 
 RUN apk add --no-cache bash bc && \
     cd /tmp/ && curl -Lskj $ES_URL | tar xzf - && \
     mv /tmp/elasticsearch-${ES_VERSION} /elastic && \
-    mkdir -p /elastic/data ${LOG_DIR} && \
+    mkdir -p /elastic/data /elastic/logs && \
     adduser -D elastic && \
     chown -Rf elastic /elastic && \
     find /elastic -iname '*.exe' -delete -or -iname '*.bat' -delete
--- run.sh
+++ run.sh
@@ -2,16 +2,13 @@
 
 [ "$HEAP_SIZE" == "auto" ] && {
   total_mem=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
-  alloc_bytes=$(echo "$total_mem * .9" | bc | cut -f1 -d\.)
+  alloc_bytes=$(echo "$total_mem * .7" | bc | cut -f1 -d\.)
   export HEAP_SIZE="$((alloc_bytes /1024/1024))m"
 }
 
-ES_JAVA_OPTS+="-Xms${HEAP_SIZE} -Xmx${HEAP_SIZE}"
+export ES_JAVA_OPTS+="-Xms${HEAP_SIZE} -Xmx${HEAP_SIZE}"
 
 echo "Starting Elasticsearch..."
 echo "ES_JAVA_OPTS: $ES_JAVA_OPTS"
 
-exec /opt/elasticsearch/bin/elasticsearch -Des.logger.level=$ELASTICSEARCH_LOG_LEVEL \
-                                          -Des.default.path.logs=${LOG_DIR} \
-                                          -Djava.net.preferIPv4Stack=true \
-                                          -Djava.net.preferIPv4Addresses
+exec /elastic/bin/elasticsearch
